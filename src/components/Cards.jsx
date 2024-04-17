c3719d12aade297edeadf2d5ab6555de09632e20
Nicolas <contact@nicolasrichelet.com>
2024-04-17 22:28:24 +0200

    Ajout des composants de base et css

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,30 @@
+import React, { useState } from 'react'
+
+import projectsData from '../datas/projects.json'
+import '../styles/cards.scss'
+
 function Cards() {
-    return
+    const [jsonData] = useState(projectsData)
+
+  // Vérifiez si l'ID du logement existe dans les données JSON
+//   const isProjectIdValid = (id) => jsonData.some((project) => project.id === id)
+    return (
+        <ul className="cards">
+          {jsonData.map((project, id) => (
+            <li key={id} className="cards__projects">
+                <img
+                  className="cards__project__image"
+                  src={project.cover}
+                  alt={project.title}
+                />
+                <div className="cards__project__gradient"></div>
+                <div className="cards__project__title">{project.title}</div>
+            </li>
+          ))}
+          {/* Si aucun logement n'a été trouvé pour un ID donné, rediriger directement vers la page 404 */}
+          {/* {!isProjectIdValid && <Link to="/404" style={{ display: 'none' }} />} */}
+        </ul>
+      )
 }
+
+export default Cards
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,18 @@
-import Cards from './components/Cards';
+import Cards from '../components/Cards';
+
+function Home() {
+    return (
+        <div>
+        <div className='container-description'>
+            <p>test</p>
+        </div>
+        <div className='container-main'>
+            <Cards />
+        </div>
+        <div className='container-contact'>
+        </div>
+        </div>
+    )
+}
+
+export default Home
