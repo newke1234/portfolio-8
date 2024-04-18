2cb5f5ba008e2cd36f1e443f32bb5d7ff74004d7
Nicolas <contact@nicolasrichelet.com>
2024-04-18 21:25:17 +0200

    Mise en place d'une condition en fonction  du type de cube (projet ou autre ?)

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,27 +1,26 @@
 import React, { useState } from 'react'
-
-import projectsData from '../datas/projects.json'
+import cardsData from '../datas/cards.json'
 import '../styles/cards.scss'
 
 function Cards() {
-    const [jsonData] = useState(projectsData)
+    const [jsonData] = useState(cardsData)
 
-  // Vérifiez si l'ID du logement existe dans les données JSON
-//   const isProjectIdValid = (id) => jsonData.some((project) => project.id === id)
     return (
         <ul className="cards">
-          {jsonData.map((project, id) => (
+          {jsonData.map((card, id) => (
             <li key={id} className="cards__projects">
+                {card.type === 'project' && (
                 <img
                   className="cards__project__image"
-                  src={project.cover}
-                  alt={project.title}
+                  src={card.cover}
+                  alt={card.title}
                 />
-                <div className="cards__project__gradient"></div>
-                <div className="cards__project__title">{project.title}</div>
+                )}
+                {/* <div className="cards__project__gradient"></div> */}
+                {/* <div className="cards__project__title">{card.title}</div> */}
             </li>
           ))}
-          {/* Si aucun logement n'a été trouvé pour un ID donné, rediriger directement vers la page 404 */}
+          {/* Si aucun projet n'a été trouvé pour un ID donné, rediriger directement vers la page 404 */}
           {/* {!isProjectIdValid && <Link to="/404" style={{ display: 'none' }} />} */}
         </ul>
       )
