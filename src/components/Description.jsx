abd73481457fc81d11ba81b639e56ebec8500b40
Nicolas <contact@nicolasrichelet.com>
2024-04-22 23:11:57 +0200

    Ajout gestion des tags

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -7,17 +7,22 @@ function Description({ details }) {
     };
 
     return (
-        <div className="description-container">
+        <div className="description__container">
             <h2>{details.title}</h2>
             <h3>{details.subtitle}</h3>
+            <div className="description__container-images">
             {details.pictures && details.pictures.map((picture, index) =>
-                picture ? <div className="description-container-images"><img key={index} src={getImagePath(picture)} alt={`${details.title} ${index + 1}`} /></div> : null
+                picture ? <img key={index} src={getImagePath(picture)} alt={`${details.title} ${index + 1}`} />: null
             )}
+            </div> 
             {details.description && details.description.map((desc, index) => (
                 <p key={index}>{desc}</p>
             ))}
-            <p className="inverse">{details.tech}</p>
-            
+            <div className="tech-container">
+                {details.tech && details.tech.map((techItem, index) => (
+                <span className="tech-tags" key={index}>{techItem}</span>
+            ))}
+            </div>
         </div>
     );
 }
