7dbea9184a146280913b12c7abc1c4fca53ad5d3
Nicolas <contact@nicolasrichelet.com>
2024-04-30 14:20:58 +0200

    test de liaison avec l'api de base

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,20 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// import dotenv from 'dotenv';
 import cardsData from '../datas/cards.json';
 import '../styles/cards.scss';
 
 function Cards({ setProjectDetails, modalOpen }) { // Receive modalOpen prop
     const [jsonData] = useState(cardsData);
+    const [selectedProject, setSelectedProject] = useState();
 
+    useEffect(() => {
+        if (!selectedProject) return;  // Ne rien faire si aucun projet n'est sélectionné
+
+        const fetchData = async () => {
+            const apiURL = `https://www.nicolasrichelet.dev/htdocs/api/index.php/projects?title=${selectedProject.title}`;
+            try {
+                const response = await axios.get(apiURL, {
+                    headers: { "DOLAPIKEY": process.env.REACT_APP_DOLAPIKEY }
+                });
+                if (response.data && response.data.length > 0) {
+                    setProjectDetails(response.data[0]);  // Assumer que le premier résultat est le bon
+                }
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        };
+
+        fetchData();
+    }, [selectedProject]); // Effect déclenché par le changement de selectedProject
+    
     const handleCardClick = card => {
-      if (card.type === "project") {
-          setProjectDetails({
-              title: card.title,
-              subtitle: card.subtitle,
-              description: card.description,
-              pictures: card.pictures || [],
-              tech: card.tech
-          });
-      }
+        if (card.type === "project") {
+            setSelectedProject(card);  // Définir le projet sélectionné basé sur la carte cliquée
+        }
     };
 
     const getImagePath = (filename) => {
