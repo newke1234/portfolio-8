ebfcb46bc421bdd94408aa656ee10030f9d5c755
Nicolas <contact@nicolasrichelet.com>
2024-06-01 13:55:30 +0200

    MAJ bio / recup info dolbarr / user

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,35 +1,39 @@
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DOMPurify from 'dompurify';
+
+
 function Bio() {
+    const [userBio, setUserBio] = useState([]);
+    
+    useEffect(() => {
+        const fetchUserBio = async () => {
+            const apiUserBio = `${process.env.REACT_APP_BASEURL}${process.env.REACT_APP_URLAPI}users/2`;
+            try {
+                const response = await axios.get(apiUserBio, {
+                    headers: { "DOLAPIKEY": process.env.REACT_APP_DOLAPIKEY }
+                });
+                if (response.data) {
+                    setUserBio([response.data]); // Wrapping the object in an array
+                    if ( response.data.length > 0) {
+                        setUserBio(response.data);
+                    }
+                }
+            } catch (error) {
+                console.error("Error fetching data for bio: ", error);
+            }
+        };
+
+        fetchUserBio();
+    }, []); 
+    
     return (
-        <div>
-            <article>Professionnel polyvalent dans le développement web, l'illustration, et la musique. Mon parcours unique croise divers domaines créatifs et techniques, offrant une perspective riche et une approche innovante à chaque projet.</article>
-
-            <h2>Formation Académique</h2>
-                <article>
-                - <u>American School of Modern Music</u>: Diplômé en section Piano Jazz.<br/>
-                - <u>Conservatoire de Musique du Val Maubué</u>: Fin d'étude en solfège et prix en Piano Jazz.<br/>  
-                - <u>Itecom Art Design</u>: Formation en design et arts visuels, game design<br/>  
-                - <u>OpenClassrooms</u>: Certification en Développement Web.<br/>
-                </article>
-
-            <h2>Expérience Professionnelle  </h2>
-                <article>
-                    - <u>Architecture-Studio (Paris)</u>: j'ai commencé mon stage en tant developppeur web : j'ai réalisé le site web complet de l'agence (PHP/MySQL, HTML/CSS/Flash), incluant un back-end pour faciliter la mise à jour de la base de données projets. Par la suite, j'ai prolongé mon stage en intégrant l'équipe d'illustrateur et j'ai pu me perfectionner dans le domaine de l'illustration d'architecture.<br/>
-                    - <u>Atelier 4 (Clermont-Ferrand)</u>: Fort de cette expérience et de cete polyvalence, j'ai été récruté comme Illustrateur et développeur web, je gérais le site en alternance avec les illustrations pour les rendus de concours d'architecture.<br/>
-                    - <u>V-Pictures: En tant qu'illustrateur</u>, chef de projet, et directeur technique, j'ai dirigé des projets complexes en collaboration avec les équipes d'illustrateurs et j'ai assuré la maintenance des équipements techniques.<br/>
-                    -<u> Freelance</u>: Depuis fin 2010, je travaille en tant qu'indépendant, réalisant des illustrations pour des architectes, avec des œuvres publiées dans des revues et magazines internationaux tels que Digital Art Masters, Exposé, 3DArtist, et 3DWorld.<br/>
-                </article>
-
-            <h2>Compétences Techniques</h2>  
-                <article>
-                    - <u>Développement</u>: HTML, CSS, Javascript, React, Node. Notions de Python, PHP et MySQL.<br/>
-                    - <u>Illustration</u>: Expertise sur Photoshop et 3DsMax(Vray / Corona).<br/>
-                    - <u>Musique</u>: Niveau professionnel en piano jazz.<br/>
-                </article>
-
-            <h2>Objectifs Professionnels</h2>  
-                <article>En revenant vers un domaine qui m'a longtemps intéressé, je suis déterminé à évoluer dans le développement, en utilisant mes compétences   interdisciplinaires, ma compréhension des domaines artistiques et techniques, et mon expérience pour apporter des solutions innovantes et efficaces.</article>
+            <div>
+            {userBio.map(bio => (
+                <p key={bio.id} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(bio.note_public) }}></p>
+            ))}
         </div>
-    )
-}
+    )};
 
 export default Bio
